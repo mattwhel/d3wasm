b693afe11e26cad7dce4ca334c1d00f4357218ba
gab <contact@gabrielcuvillier.pro>
2018-12-31 18:37:27 +0100

    Discard ARB2 calls

--- neo/renderer/RenderSystem_init.cpp
+++ neo/renderer/RenderSystem_init.cpp
@@ -754,10 +754,13 @@ void R_InitOpenGL( void ) {
 
 	// parse our vertex and fragment programs, possibly disably support for
 	// one of the paths if there was an error
+#ifndef __EMSCRIPTEN__
+#else
 	R_ARB2_Init();
 
 	cmdSystem->AddCommand( "reloadARBprograms", R_ReloadARBPrograms_f, CMD_FL_RENDERER, "reloads ARB programs" );
 	R_ReloadARBPrograms_f( idCmdArgs() );
+#endif
 
 	// allocate the vertex array range or vertex objects
 	vertexCache.Init();
