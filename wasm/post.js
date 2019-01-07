57326da19803cef3d3e91e9f3557a9c798c782d9
gab <contact@gabrielcuvillier.pro>
2019-01-07 21:51:17 +0100

    Updated post.js with the two files to fetch demo_chunk00 adn demo_chunk01

--- wasm/post.js
+++ wasm/post.js
@@ -5,6 +5,20 @@
   script.onload = function(){
     // remote script has loaded
   };
-  script.src = 'demo_base.js';
+  script.src = 'demo_chunk00.js';
   d.getElementsByTagName('head')[0].appendChild(script);
 }(document));
+
+(function(d, script) {
+  script = d.createElement('script');
+  script.type = 'text/javascript';
+  script.async = false;
+  script.onload = function(){
+    // remote script has loaded
+  };
+  script.src = 'demo_chunk01.js';
+  d.getElementsByTagName('head')[0].appendChild(script);
+}(document));
+
+//python $EMSCRIPTEN/tools/file_packager.py demo_chunk00.data --preload data/demo_chunk00.pk4@/usr/local/share/dhewm3/base/ --js-output=demo_chunk00.js --use-preload-cache --no-heap-copy
+//python $EMSCRIPTEN/tools/file_packager.py demo_chunk01.data --preload data/demo_chunk01.pk4@/usr/local/share/dhewm3/base/ --js-output=demo_chunk01.js --use-preload-cache --no-heap-copy
