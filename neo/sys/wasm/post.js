a587885f76bcc125cfd37fff375c5cd8e1d32754
gab <contact@gabrielcuvillier.pro>
2019-01-23 18:13:31 +0100

    Restored usage of non-chunked demo data, easier to setup

--- neo/sys/wasm/post.js
+++ neo/sys/wasm/post.js
@@ -5,7 +5,7 @@
   script.onload = function(){
     // remote script has loaded
   };
-  script.src = 'demo_game00.js';
+  //script.src = 'demo_game00.js';
   d.getElementsByTagName('head')[0].appendChild(script);
 }(document));
 
