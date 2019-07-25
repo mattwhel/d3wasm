fe868c7c56f1597ffd9caac9e5528b51d4ec89d9
Gabriel <gabriel.cuvillier@continuation-labs.com>
2019-07-25 09:34:52 +0200

    Added packaging scripts for windows

---
+++ neo/sys/wasm/package_chunked_demo_data.bat
@@ -0,0 +1,3 @@
+python %EMSDK%/upstream/emscripten/tools/file_packager.py demo_bootstrap.data --preload %1/demo_bootstrap.pk4@/usr/local/share/d3wasm/base/demo_bootstrap.pk4 --js-output=demo_bootstrap.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py demo_game00.data --preload %1/demo_game00.pk4@/usr/local/share/d3wasm/base/demo_game00.pk4 --js-output=demo_game00.js --use-preload-cache --no-heap-copy
+python %EMSDK%/upstream/emscripten/tools/file_packager.py demo_game01.data --preload %1/demo_game01.pk4@/usr/local/share/d3wasm/base/demo_game01.pk4 --js-output=demo_game01.js --use-preload-cache --no-heap-copy
---
+++ neo/sys/wasm/package_demo_data.bat
@@ -0,0 +1,1 @@
+python %EMSDK%/upstream/emscripten/tools/file_packager.py demo00.data --preload %1/demo00.pk4@/usr/local/share/d3wasm/base/demo00.pk4 --js-output=demo00.js --use-preload-cache --no-heap-copy
